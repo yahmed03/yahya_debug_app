c0695891cf368356b3d40b904b2827996a8c2ec6
yahmed03 <yahya.ahmed@solirius.com>
2025-09-24 14:14:41 +0100

    final changes: signout button db change sign in button css fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_24_115231) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_24_120819) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_24_115231) do
     t.bigint "user_id"
     t.text "answer"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "score", default: 0
     t.integer "percentage"
     t.boolean "completed"
